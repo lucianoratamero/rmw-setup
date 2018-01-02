2511ef3dd73bc51f4ef06218c66a3b265c48cbd5
Luciano Ratamero <luciano@ratamero.com>
2018-01-02 12:08:07 -0200

    updates eslintrc to work better with react.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,10 @@
 module.exports = {
-    "extends": "airbnb"
+    "extends": "airbnb",
+    "rules": {
+      "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+      "react/prefer-stateless-function": [0]
+    },
+    "globals": {
+      "document": false
+    }
 };
